22e7bda030cf8992d310b07b0b332bd5fa927a59
rsninja722 <jnick722547@gmail.com>
2020-02-28 13:29:06 -0500

    key input

---
+++ gamej/src/game/Input.java
@@ -0,0 +1,140 @@
+package game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import game.drawing.Camera;
+import game.drawing.Draw;
+import game.physics.Physics;
+import game.physics.Point;
+import game.Utils;
+
+
+public class Input {
+
+    public static Point rawMousePos = new Point(0,0);
+    public static Point mousePos = new Point(0,0);
+
+    private static byte[] mouseInputs = new byte[3]; 
+    private static byte[] keyInputs = new byte[256];
+
+    public static void init() {
+        System.out.println("[Input] initializing");
+
+        // Initialize input arrays 
+        for(int i=0;i<mouseInputs.length;i++) {
+            mouseInputs[i] = 0;
+        }
+        for(int i=0;i<keyInputs.length;i++) {
+        	keyInputs[i] = 0;
+        }
+
+        // key Presses
+        Draw.frame.addKeyListener(new KeyListener() {
+        	 @Override
+            public void keyPressed(KeyEvent e) {
+        		 keyInputs[e.getKeyCode()] = 2;
+            }
+            @Override
+            public void keyReleased(KeyEvent e) { 
+            	keyInputs[e.getKeyCode()] = 0;
+        	}
+			@Override
+			public void keyTyped(KeyEvent e) {}
+        });
+        
+        // mouse presses
+        Draw.panel.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                mouseInputs[e.getButton()-1] = 2;
+            }
+            public void mouseReleased(MouseEvent e) {
+                mouseInputs[e.getButton()-1] = 0;
+            }
+        });
+        
+        // mouse movement
+        Draw.panel.addMouseMotionListener(new MouseMotionAdapter() {
+        	@Override
+            public void mouseMoved(MouseEvent e) {
+                setMousePosition(e.getX(), e.getY());
+            }
+        	@Override
+            public void mouseDragged(MouseEvent e) {
+                setMousePosition(e.getX(), e.getY());
+            }
+        });
+            
+    }
+
+    private static void setMousePosition(int x,int y) {
+        rawMousePos.x = x;
+        rawMousePos.y = y;
+
+        if(Draw.drawingMode==0) {
+            mousePos.x = x-Camera.x;
+            mousePos.y = y-Camera.y;
+        } else if(Draw.drawingMode==1) {
+            double xoff = GameJava.gw/2;
+            double yoff = GameJava.gh/2;
+            mousePos.x = ((x-xoff)/Camera.zoom+xoff)-Camera.x;
+            mousePos.y = ((y-yoff)/Camera.zoom+yoff)-Camera.y;
+        } else {
+            double xoff = GameJava.gw/2;
+            double yoff = GameJava.gh/2;
+            Point tempPos = new Point(((x-xoff)/Camera.zoom+xoff)-Camera.x,((y-yoff)/Camera.zoom+yoff)-Camera.y);
+    
+            Point center = new Point(-Camera.x + GameJava.gw/2, -Camera.y + GameJava.gh/2);
+            double tempAngle = Utils.pointTo(center,tempPos) - Camera.angle; 
+            double tempDist = Physics.dist(center,tempPos);
+    
+            mousePos.x = center.x + (Math.cos(tempAngle) * tempDist);
+            mousePos.y = center.y + (Math.sin(tempAngle) * tempDist);
+        }
+    }
+
+    // needs to be called every cycle to make sure clicks are registered properly 
+    public static void manageInput() {
+        for(int i=0;i<mouseInputs.length;i++) {
+            if(mouseInputs[i] == 2) {
+                mouseInputs[i] = 1;
+            }
+        }
+        for(int i=0;i<keyInputs.length;i++) {
+            if(keyInputs[i] == 2) {
+            	keyInputs[i] = 1;
+            }
+        }
+    }
+
+    /** @param buttonID 0 = lmb, 1 = middle click, 2 = rmb @return if that mouse button is held down  */
+    public static boolean mouseDown(int buttonID) {
+        return mouseInputs[buttonID] > 0;
+    }
+    
+    /** @param buttonID 0 = lmb, 1 = middle click, 2 = rmb @return only true the first cycle the button is held down */
+    public static boolean mouseClick(int buttonID) {
+        return mouseInputs[buttonID] == 2;
+    }
+    
+    /** @param id of a key @return only true the first cycle the button is held down */
+    public static boolean keyClick(int keyID) {
+        return keyInputs[keyID] == 2;
+    }
+    /** @param keyCode.somekey @return only true the first cycle the key is held down */
+    public static boolean keyClick(KeyCodes keyID) {
+        return keyInputs[keyID.getCode()] == 2;
+    }
+    
+    /** @param id of a key @return if key is held */
+    public static boolean keyDown(int keyID) {
+        return keyInputs[keyID] > 0;
+    }
+    /** @param keyCode.somekey @return if key is held */
+    public static boolean keyDown(KeyCodes keyID) {
+        return keyInputs[keyID.getCode()] > 0;
+    }
+}
---
+++ gamej/src/game/LoopManager.java
@@ -0,0 +1,49 @@
+package game;
+
+import game.drawing.Draw;
+
+public class LoopManager {
+
+    private static GameJava mainGameClass;
+
+    // calculation for how long to wait between each frame
+	public static final double nanosecondsPerSecond = 1000000000;
+	public static double nanosPerFrame;
+	private static double lastDrawTime = System.nanoTime();
+	
+    public static double nanosPerUpdate;
+    private static double lastUpdateTime = System.nanoTime();
+
+    public static void startLoops(GameJava mainGameObject) {
+        System.out.println("[LoopManager] starting loops with: " + mainGameObject.toString());
+        mainGameClass = mainGameObject;
+
+        nanosPerFrame = nanosecondsPerSecond / GameJava.framePerSecond;
+        nanosPerUpdate = nanosecondsPerSecond / GameJava.updatesPerSecond;
+		
+		while(GameJava.running) {
+			
+			// drawing
+			double currentTime = System.nanoTime();
+			if(currentTime - lastDrawTime >= nanosPerFrame) {
+                synchronized(Draw.panel) {
+                	Draw.preRender();
+                	mainGameClass.draw();
+                    Draw.render();
+                }
+				lastDrawTime = currentTime;
+				GameJava.frameCount++;
+			}
+			
+			// updating
+			currentTime = System.nanoTime();
+			if(currentTime - lastUpdateTime >= nanosPerUpdate) {
+				mainGameClass.update();
+                lastUpdateTime = currentTime;
+                GameJava.updateCount++;
+                Input.manageInput();
+			}
+			
+		}
+	}
+}
